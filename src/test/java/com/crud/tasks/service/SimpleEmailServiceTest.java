b86e8c1cfe0108378b1a28ea2a1d58ef9e7aefc2
darosg1 <darosg1@yahoo.com>
2020-05-16 16:18:14 +0200

    Heroku3

--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -23,7 +23,7 @@ public class SimpleEmailServiceTest {
     @Test
     public void shouldSendEmail(){
     //given
-    Mail mail = new Mail("test@test.com", "Test", "Test message", "test1@test.com");
+    Mail mail = new Mail("test@test.com", "Test", "Test message");
 
     SimpleMailMessage mailMessage = new SimpleMailMessage();
     mailMessage.setTo(mail.getMailTo());
